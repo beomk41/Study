e78ae835f4b06500f3320c129a93657bdb553808
Beomsu <81428281+beomk41@users.noreply.github.com>
2023-09-02 13:16:53 +0900

    [Gold IV] Title: 오큰수, Time: 904 ms, Memory: 190664 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Stack<Integer> stack = new Stack<>();
+    static int N, arr[];
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+        for(int num : arr){
+            bw.write(num+" ");
+        }
+        bw.flush();
+        }
+    }
