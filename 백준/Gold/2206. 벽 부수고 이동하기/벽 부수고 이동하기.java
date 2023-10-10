8a884024b8d60aa56e3e70a5fd1212631565c094
Beomsu <81428281+beomk41@users.noreply.github.com>
2023-10-10 14:21:19 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 688 ms, Memory: 93292 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M, K, map[][], min = Integer.MAX_VALUE;
+    static boolean[][][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = 1;
+
+        map = new int[N][M];
+        visited = new boolean[N][M][K+1];
+        for(int i = 0; i < N; i++) {
+            String tmp = br.readLine();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = tmp.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0, 0);
+
+        if(min == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(min);
+    }
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static class Info extends Point {
+        int cnt;
+
+        public Info(int x, int y, int cnt) {
+            super(x, y);
+            this.cnt = cnt;
+        }
+    }
+
+    private static void bfs(int x, int y, int cnt) {
+        Queue<Info> q = new ArrayDeque<>();
+        q.offer(new Info(x,y,cnt));
+        visited[x][y][cnt] = true;
+
+        int level = 1;
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                Info tmp = q.poll();
+
+                if(tmp.x == N-1 && tmp.y == M-1) {
+                    min = level;
+                    return;
+                }
+
+                for(int d = 0; d < 4; d++) {
+                    int nx = tmp.x + dx[d];
+                    int ny = tmp.y + dy[d];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+                    if(map[nx][ny] == 0 && !visited[nx][ny][tmp.cnt]) {
+                        visited[nx][ny][tmp.cnt] = true;
+                        q.offer(new Info(nx,ny,tmp.cnt));
+                    }
+
+                    if(map[nx][ny] == 1 && tmp.cnt < K && !visited[nx][ny][tmp.cnt+1] ) {
+                        visited[nx][ny][tmp.cnt+1] = true;
+                        q.offer(new Info(nx,ny,tmp.cnt+1));
+                    }
+                }
+            }
+            level++;
+        }
+    }
+}
