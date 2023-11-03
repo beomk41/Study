ba0274cb810b52c60e0b0d5add24f20b5372ccaa
Beomsu <81428281+beomk41@users.noreply.github.com>
2023-11-03 23:55:59 +0900

    [Silver III] Title: N과 M (2), Time: 84 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static int[] arr;
+	public static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+
+		dfs(1, 0);
+
+	}
+
+	public static void dfs(int at, int depth) {
+		 
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+        
+		for (int i = at; i <= N; i++) {
+ 
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+ 
+		}
+	}
+}
