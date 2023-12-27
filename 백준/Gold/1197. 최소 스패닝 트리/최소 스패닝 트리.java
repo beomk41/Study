3a9c1c8d237523c323bbe3cef51ad6dad93c251a
Beomsu <81428281+beomk41@users.noreply.github.com>
2023-12-27 19:06:21 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 484 ms, Memory: 46960 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int s;
+    int e;
+    int cost;
+
+    public Node (int s, int e, int cost) {
+        this.s = s;
+        this.e = e;
+        this.cost = cost;
+    }
+
+    public int compareTo(Node o) {
+        return Double.compare(this.cost, o.cost);
+    }
+}
+
+public class Main {
+    // BJ4386
+    static int V, E;
+    static int[] parent;
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+    static int min;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            pq.add(new Node(s, e, c));
+        }
+
+        set();
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if(find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+                min += now.cost;
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) parent[y] = x;
+            else parent[x] = y;
+        }
+    }
+
+    private static int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void set() {
+        parent = new int[V+1];
+        for (int i=0; i<V; i++) {
+            parent[i] = i;
+        }
+    }
+}
